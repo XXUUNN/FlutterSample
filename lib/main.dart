0441bf72ddb170698167dfef2413f08afcb06eba
XUN <gong_xun@foxmail.com>
2018-08-24 18:05:07 +0800

    drawer appBar

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,7 @@ class RandomWordsState extends State<RandomWords> {
       new MaterialPageRoute(
         builder: (context) {
           final tiles = _saved.map(
-            (pair) {
+                (pair) {
               return new ListTile(
                 title: new Text(
                   pair.asPascalCase,
@@ -93,9 +93,9 @@ class RandomWordsState extends State<RandomWords> {
           );
           final divided = ListTile
               .divideTiles(
-                context: context,
-                tiles: tiles,
-              )
+            context: context,
+            tiles: tiles,
+          )
               .toList();
           return new Scaffold(
             appBar: new AppBar(
@@ -144,7 +144,9 @@ class UITestApp extends StatelessWidget {
     );
 
     Column buildButtonColumn(IconData icon, String label) {
-      var color = Theme.of(context).primaryColor;
+      var color = Theme
+          .of(context)
+          .primaryColor;
       return new Column(
         children: <Widget>[
           new Icon(
@@ -231,26 +233,58 @@ class ShowHideWidget extends StatefulWidget {
 
 class ShowHideState extends State<ShowHideWidget> {
   bool isShow = true;
+  bool isPress = false;
 
   switchState() {
     setState(() {
+      isPress = true;
       isShow = !isShow;
     });
+  }
+
+  /**
+   * 显示dialog
+   * dart下划线开头表示私有  相当于private
+   */
+  _myShowDialog() {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+            title: new Text('title'),
+            content: new Text('content'),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text('cancle'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
   }
 
   _getChild() {
     if (isShow) {
       return new Container(
-        padding:EdgeInsets.only(left: -0.0) ,
+          padding: EdgeInsets.only(left: -0.0),
           child: new Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-            new Text('666'),
-            new Text('77777\n7777'),
-            new Image.network(
-                "https://ss3.baidu.com/9fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=c4b4d6dff01f4134ff37037e151e95c1/c995d143ad4bd113115afc0e57afa40f4bfb0572.jpg")
-          ]));
+//            new Text('666'),
+//            new Text('77777\n7777'),
+//                new Image.network(
+//                  "https://ss3.baidu.com/9fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=c4b4d6dff01f4134ff37037e151e95c1/c995d143ad4bd113115afc0e57afa40f4bfb0572.jpg",
+//                  width: 50.0,
+//                ),
+                new RaisedButton.icon(
+                    onPressed: _myShowDialog,
+                    icon: new Icon(Icons.print),
+                    label: new Text("raiseBtn"))
+              ]));
     } else {
       return new Text('666');
     }
@@ -258,9 +292,20 @@ class ShowHideState extends State<ShowHideWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    Scaffold scaffold = new Scaffold(
       appBar: new AppBar(
         title: new Text('showHide'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.update), onPressed: null),
+          new PopupMenuButton(
+            itemBuilder: (BuildContext context) {
+              var item = new PopupMenuItem(child: new Text('fff'));
+              List<PopupMenuItem> list = List.from([item]);
+              list.add(item);
+              return list;
+            },
+          )
+        ],
       ),
       backgroundColor: Colors.yellow,
       body: new Center(
@@ -268,9 +313,13 @@ class ShowHideState extends State<ShowHideWidget> {
       ),
       floatingActionButton: new FloatingActionButton(
         onPressed: switchState,
-        tooltip: 'showAndHide Text',
+        tooltip: 'showAndHide',
         child: new Icon(Icons.update),
       ),
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      drawer: new Drawer(),
     );
+
+    return scaffold;
   }
 }
